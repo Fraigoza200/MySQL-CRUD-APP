42bee3b81c903c64b6c16da00be1647ec1063fe9
Francisco Raigoza <raigoza.frankie@gmail.com>
2022-10-11 23:23:43 -0700

    Fixed React Bug, Retrieved books from backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,39 +1,40 @@
-import { Description } from '@material-ui/icons'
-import express from 'express'
-import mysql from "mysql"
-const app = express()
+import express from 'express';
+import mysql from 'mysql';
+import cors from 'cors';
+
+const app = express();
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "groot",
-  database:"bookdb"
-})
+  host: 'localhost',
+  user: 'root',
+  password: 'groot',
+  database: 'bookdb',
+});
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
-app.get("/", (req,res) => {
-  res.json("this is the backend")
-})
+app.get('/', (req, res) => {
+  res.json('this is the backend');
+});
 
-app.get("/books", (req,res) => {
-  const q = "SELECT * FROM books"
+app.get('/books', (req, res) => {
+  const q = 'SELECT * FROM books';
 
-  db.query(q,(err,data) => {
-    if(err) return res.json(err)
-    return res.json(data)
-  })
-})
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
 
-app.post("/books", (req,res) => {
-  const q = "INSERT INTO books (`title`, `desc`,`cover`) VALUES (?)"
-  const values = [req.body.title, req.body.desc, req.body.cover]
-
+app.post('/books', (req, res) => {
+  const q = 'INSERT INTO books (`title`, `desc`,`cover`) VALUES (?)';
+  const values = [req.body.title, req.body.desc, req.body.cover];
 
   db.query(q, [values], (err, data) => {
-    if(err) return res.json(err)
-    return res.json("book has been created successfully")
-  })
-})
+    if (err) return res.json(err);
+    return res.json('book has been created successfully');
+  });
+});
 
-app.listen(3000, () => console.log("running on port 3000"))
+app.listen(3000, () => console.log('running on port 3000'));
--- Client/src/pages/Books.jsx
+++ Client/src/pages/Books.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Books = () => {
-  return (
-    <div>Books</div>
-  )
-}
+  const [books, setBooks] = useState([]);
 
-export default Books
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/books');
+        setBooks(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchAllBooks();
+  }, []);
+
+  return (
+    <div>
+      <h1>Books In Library</h1>
+      <div className="books">
+        {books.map((book) => (
+          <div className="book" key={book.id}>
+            {book.cover && <img src={book.cover} alt="" />}
+            <h2>{book.title}</h2>
+            <p>{book.desc}</p>
+            <span>{book.price}</span>
+          </div>
+        ))}
+        <button><Link to="/add"> Add New Book </Link></button>
+      </div>
+    </div>
+  );
+};
+
+export default Books;
